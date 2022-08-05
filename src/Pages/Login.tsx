5fd8a026c534e85d87808351c67fc55ba7e48e0b
max-programming <ui.sabuwala@gmail.com>
2022-08-05 23:40:55 +0530

    🔥 Remove unnecessary code

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -6,69 +6,17 @@ import {
   Paper,
   PaperProps,
   Stack,
-  createStyles,
   Container,
   Center,
   Anchor,
   Group,
   Button,
 } from '@mantine/core';
-import { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import AuthInput from '../Components/AuthInput';
 
-const useStyles = createStyles(
-  (
-    theme,
-    { floating, floating1 }: { floating: boolean; floating1: boolean }
-  ) => ({
-    root: {
-      position: 'relative',
-    },
-
-    label: {
-      position: 'absolute',
-      zIndex: 2,
-      top: 7,
-      left: theme.spacing.sm,
-      pointerEvents: 'none',
-      color:
-        floating || floating1
-          ? theme.colorScheme === 'dark'
-            ? theme.white
-            : theme.black
-          : theme.colorScheme === 'dark'
-          ? theme.colors.dark[3]
-          : theme.colors.gray[5],
-      transition:
-        'transform 150ms ease, color 150ms ease, font-size 150ms ease',
-      transform:
-        floating || floating1
-          ? `translate(-${theme.spacing.sm}px, -28px)`
-          : 'none',
-      fontSize: floating || floating1 ? theme.fontSizes.xs : theme.fontSizes.sm,
-      fontWeight: floating || floating1 ? 500 : 400,
-    },
-
-    required: {
-      transition: 'opacity 150ms ease',
-      opacity: floating ? 1 : 0,
-    },
-
-    input: {
-      '&::placeholder': {
-        transition: 'color 150ms ease',
-        color: !floating ? 'transparent' : undefined,
-      },
-    },
-  })
-);
-
 export default function Login(props: PaperProps) {
-  const [focused, setFocused] = useState(false);
-  const [focused1, setFocused1] = useState(false);
-
   const form = useForm({
     initialValues: {
       username: '',
@@ -82,10 +30,6 @@ export default function Login(props: PaperProps) {
           ? 'Password should include at least 6 characters'
           : null,
     },
-  });
-  const { classes } = useStyles({
-    floating: form.values.username.trim() !== '' || focused,
-    floating1: form.values.password.trim() !== '' || focused1,
   });
 
   return (
