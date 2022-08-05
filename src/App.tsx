956804fb6dbe68e1d2d0bb625cc465bae41f6dfe
max-programming <ui.sabuwala@gmail.com>
2022-08-05 15:23:35 +0530

    Added React Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Demo from './Components/Modal';
 import { AppShell } from '@mantine/core';
 import { useState } from 'react';
 import HeaderTabsColored from './Components/DS_Header';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,14 +10,18 @@ function App() {
   return (
     <div className='App'>
       <AppShell>
-        <HeaderTabsColored
-          user={{
-            name: '',
-            image: '',
-          }}
-          tabs={['Dashboard', 'Insights', 'About']}
-        />
-        <Demo />
+        <Routes>
+          <Route path='/'>
+            <HeaderTabsColored
+              user={{
+                name: '',
+                image: '',
+              }}
+              tabs={['Dashboard', 'Insights', 'About']}
+            />
+            <Demo />
+          </Route>
+        </Routes>
       </AppShell>
     </div>
   );
