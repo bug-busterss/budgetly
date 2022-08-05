087d7b20e1a02ae6ed7d8df9e9e03d7d85aff63b
Faizan <faizanlal563@gmail.com>
2022-08-05 15:27:04 +0530

    Updated card

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,11 +1,19 @@
-import { Card, Title } from "@mantine/core";
+import { Card, Title } from '@mantine/core';
 
-export default function ExpenseCard() {
+interface HeaderTabsProps {
+  user: { name: string; image: string };
+  tabs: string;
+}
+
+export default function ExpenseCard({ user, tabs }: HeaderTabsProps) {
   return (
     <Card>
-      <Card.Section>
-        <Title>ijdi0ide</Title>
-      </Card.Section>
+      <Card shadow='sm' p='lg' radius='md' withBorder>
+        <Title>Expense </Title>
+        <Title order={2}>$600</Title>
+      </Card>
+
+      <Card.Section></Card.Section>
     </Card>
   );
 }
