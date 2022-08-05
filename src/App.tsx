7e899b45ef4ce6d8d06e2dac7e8fae65eb886615
Uraniumking007 <Uraniumking007@users.noreply.github.com>
2022-08-05 15:46:33 +0530

    added login

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { useState } from 'react';
 import HeaderTabsColored from './Components/DS_Header';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className='App'>
       <AppShell>
