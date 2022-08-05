e9269b489f0b9bb6c5aa0d642117d83cc0089de9
Faizan <faizanlal563@gmail.com>
2022-08-05 15:49:51 +0530

    added container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Demo from './Components/Modal';
-import { AppShell } from '@mantine/core';
+import { AppShell, Container } from '@mantine/core';
 import { useState } from 'react';
 import HeaderTabsColored from './Components/DS_Header';
 import { Route, Routes } from 'react-router-dom';
@@ -24,8 +24,10 @@ function App() {
                   }}
                   tabs={['Dashboard', 'Insights', 'About']}
                 />
-                <Demo />
-                <ExpenseCard />
+                <Container>
+                  <Demo />
+                  <ExpenseCard />
+                </Container>
               </>
             }
           />
