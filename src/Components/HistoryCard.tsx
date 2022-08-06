cf3da1105cab2fee0ee5f5581bff5d7db1cf4866
Uraniumking007 <Uraniumking007@users.noreply.github.com>
2022-08-06 09:05:18 +0530

    added deleted

--- src/Components/HistoryCard.tsx
+++ src/Components/HistoryCard.tsx
@@ -1,6 +1,14 @@
-import { Card, Group, Text } from '@mantine/core';
+import {
+  ActionIcon,
+  Button,
+  Card,
+  Group,
+  Text,
+  UnstyledButton,
+} from '@mantine/core';
 import { Activity } from '../types';
 import { format } from 'timeago.js';
+import { IconTrash } from '@tabler/icons';
 
 export default function HistoryCard({ activity }: { activity: Activity }) {
   return (
@@ -9,14 +17,24 @@ export default function HistoryCard({ activity }: { activity: Activity }) {
         <Card.Section p='xl'>
           <Group position='apart'>
             <Group position='center' spacing={8}>
-              <Text transform='capitalize' size='xl'>
+              <Text transform='capitalize' size='xl' mr='md'>
                 {activity.name}
               </Text>
               <Text size='xl' weight={700}>
                 ₹{activity.amount}
               </Text>
             </Group>
-            <Text size='xl'>{format(activity.createdAt)}</Text>
+            <Group>
+              <Text size='xl'>{format(activity.createdAt)}</Text>
+
+              <ActionIcon
+                variant='transparent'
+                color='red'
+                // sx={{ '&[data-loading]': { backgroundColor: 'red' } }}
+              >
+                <IconTrash />
+              </ActionIcon>
+            </Group>
           </Group>
         </Card.Section>
       </Card>
